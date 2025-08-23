6736b6f556d3fb426197398d661a9dfa8cb08517
Raushan Raj <137853785+raushan2104@users.noreply.github.com>
2025-08-24 02:18:54 +0530

    created account drawer

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,8 +1,31 @@
-import MainLayout from '../layout';
+
+import CreateAccountDrawer from '@/components/create-account-drawer';
+import { Card, CardContent } from '@/components/ui/card';
+import { Plus } from 'lucide-react';
 import React from 'react'
 
-const Dashboard = () => {
-  return <div>Dashboard Page</div>
+const DashboardPage = () => {
+  return (
+    <div className='px-5'>
+        {/* Budget Progress */}
+
+
+        {/* Overview */}
+
+
+        {/* Accounts Grid */}
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3" >
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5"> 
+            <Plus className="h-10 w-10 mb-2" />
+            <p className="text-sm font-medium">Add New Account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+        </div>
+    </div>
+  );
 }
 
-export default Dashboard;
+export default DashboardPage;
