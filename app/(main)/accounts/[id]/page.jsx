76041b50fd4e8405a800b954e585471ad9c3e0b6
Raushan Raj <137853785+raushan2104@users.noreply.github.com>
2025-09-07 23:53:36 +0530

    added bar chart

--- app/(main)/accounts/[id]/page.jsx
+++ app/(main)/accounts/[id]/page.jsx
@@ -3,7 +3,7 @@ import { getAccountWithTransactions } from '@/actions/account';
 import { notFound } from 'next/navigation'; 
 import TransactionTable from '../_components/transaction-table';
 import { BarLoader } from 'react-spinners';
-
+import { AccountChart } from '../_components/account-chart';
 const AccountsPage = async ({ params }) => {
   const { id } = params;
   const accountsData = await getAccountWithTransactions(id);
@@ -38,6 +38,12 @@ const AccountsPage = async ({ params }) => {
       </div>
 
       {/* Chart Section */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <AccountChart transactions={transactions} />
+      </Suspense>
+      
 
       {/* Transaction Table */}
       <Suspense fallback={<BarLoader className='mt-4' width={'100%'} color='#9333ea' />}>
