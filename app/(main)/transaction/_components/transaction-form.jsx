7744d718973353e555f4a9f979cd0a44e166aebf
Raushan Raj <137853785+raushan2104@users.noreply.github.com>
2025-10-11 22:45:07 +0530

    create transaction

---
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -0,0 +1,74 @@
+'use client';
+import useFetch from '@/hooks/use-fetch';
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { transactionSchema } from "@/app/lib/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import AccountsPage from '../../accounts/[id]/page';
+
+const AddTransactionForm = () => {
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        formSate: {errors},
+        watch,
+        getValues,
+        reset,
+    }=useForm({
+        resolver: zodResolver(transactionSchema),
+        defaultValues: {
+            type : "EXPENSE",
+            amount: "",
+            description:"",
+            accountId: AccountsPage.find((ac)=> ac.isDefault)?.id,
+            date:new Date(),
+            isRecurring: false,
+        },
+    });
+
+    const {
+        loading: transactionLoading,
+        fn: transactionFn,
+        data: transactionResult,
+    } = useFetch(createTransaction);
+
+    const type = watch( "type");
+    const isRecurring = watch("isRecurring");
+    const date = watch("date");
+  return (
+    <form className='space-y-6'>
+    {/* AI receipt scanner */}
+    <div className='space-y-2'>
+      <label className='text-sm font-medium'>
+        Type
+        </label>
+        <Select 
+            onValueChange= {(value)=> setValue("type", value)}
+            defaultValue={type}
+        >
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Select type" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="EXPENSE">Expense</SelectItem>
+    <SelectItem value="INCOME">Income</SelectItem>
+  </SelectContent>
+</Select>
+    {/* {errors.type && (
+        <p className='text-sm text-red-500'>{errors.type.message}</p>
+    )} */}
+      
+    </div>
+    </form>
+  )
+}
+
+export default AddTransactionForm;
